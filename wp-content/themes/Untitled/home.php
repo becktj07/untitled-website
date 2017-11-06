da0f0a75ac75408a823e2aaee9f270519a95d4b2
Tyler Beck <becktj07@uww.edu>
2017-11-05 19:45:26 -0600

    fonts directory and .gitignore file added

--- wp-content/themes/Untitled/home.php
+++ wp-content/themes/Untitled/home.php
@@ -4,9 +4,9 @@
 <?php get_header(); ?>
 
 <section class="home-slider wrapper">
-	<div class="home-slider__slide"><img src="http://via.placeholder.com/1000x800" /></div>
-	<div class="home-slider__slide"><img src="http://via.placeholder.com/1000x800" /></div>
-	<div class="home-slider__slide"><img src="http://via.placeholder.com/1000x800" /></div>
+	<div class="home-slider__slide"><img src="http://via.placeholder.com/1000x800" class="image-contain" /></div>
+	<div class="home-slider__slide"><img src="http://via.placeholder.com/1000x800" class="image-contain" /></div>
+	<div class="home-slider__slide"><img src="http://via.placeholder.com/1000x800" class="image-contain" /></div>
 </section>	
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/homeSlider.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/scripts/vendor/slick.min.js"></script>
