5e2ea0752bda72ee5fedd7d769974d64f95e81aa
Tyler Beck <becktj07@uww.edu>
2017-11-11 19:56:02 -0600

    at a stopping point:  starting mobile animations

--- wp-content/themes/untitled/scripts/animations.js
+++ wp-content/themes/untitled/scripts/animations.js
@@ -35,19 +35,42 @@ function runFunctionsBasedOnScreenSize() {
 function desktopFunctions() {
     $('.button-trigger').click(function(e) {
         e.preventDefault();
-        $('.info-flex-container').animate({ opacity: 0 }, 300, function(){
-            $('.branding-text-overlay > div:nth-of-type(3)').animate({ opacity: 0 }, 350, function(){
-                $('.branding-text-overlay > div:nth-of-type(2)').animate({ opacity: 0 }, 400, function(){
-                    $('.branding-text-overlay > div:nth-of-type(1) > svg > g > path').css({fill: 'black', transition: '2.0s'}); 
-                    $('.branding-text-overlay > div:nth-of-type(1)').animate({ top: '50%' }, 500, function(){
-                        location.href='/untitled';
-                    });  
-                });  
-            });        
+        $('.home-slider').animate({ opacity: 0 }, 350, function(){
+            $('.info-flex-container').animate({ opacity: 1}, 500);
+                $('.home-text-overlay').animate({ top: '100px'}, 500,);
+                $('.home-text-overlay').css('z-index', -3);
+                $('.home-text-overlay > svg > g > path').css({fill: '#FFF6D6', transition: '.5s'}); 
         });
     })
 }
 
 function mobileFunctions() {
-   console.log('do nothing');
+    $('.button-trigger').click(function(e) {
+        e.preventDefault();
+        $('.home-slider').animate({ opacity: 0 }, 350, function(){
+            $('.info-flex-container').animate({ opacity: 1}, 500);
+            $('.home-text-overlay').animate({ top: '0',}, 500,);
+            $('.home-text-overlay').css('z-index', -3);
+            $('.home-text-overlay > svg > g > path').css({fill: '#FFF6D6', transition: '.5s'}); 
+        });
+    })
 };
+
+
+// function desktopFunctions() {
+//     $('.button-trigger').click(function(e) {
+//         e.preventDefault();
+//         $('.home-slider').animate({ opacity: 0 }, 250, function(){
+//             $('.info-flex-container').animate({ opacity: 0 }, 300, function(){
+//                 $('.branding-text-watermark > div:nth-of-type(3) > svg > g > path').animate({ opacity: 0 }, 350, function(){
+//                     $('.branding-text-watermark > div:nth-of-type(2) > svg > g > path').animate({ opacity: 0 }, 400, function(){
+//                         $('.branding-text-watermark > div:nth-of-type(1) > svg > g > path').css({fill: 'black', transition: '2.0s'}); 
+//                         $('.branding-text-watermark > div:nth-of-type(1)').animate({ top: '50%' }, 500, function(){
+//                             //location.href='/untitled';
+//                         });  
+//                     });  
+//                 });        
+//             });
+//         });
+//     })
+// }
