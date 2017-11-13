da40bfb855f010f4e3d4371aa63bf0aa4d2ca536
Tyler Beck <becktj07@uww.edu>
2017-11-13 16:34:18 -0600

    stopping point. second function runs after resize only.

--- wp-content/themes/untitled/scripts/animations.js
+++ wp-content/themes/untitled/scripts/animations.js
@@ -1,75 +1,73 @@
 
+var windowWidth = $(window).width();
+
 $(document).ready(function() {
-    runFunctionsBasedOnScreenSize();
+    checkSize();
+    console.log("document ready " + $(window).width());
 });
 
-var $window = $(window);
-var isMobile = false;
+$(window).resize(function() {
+    console.log("resize " + $(window).width());
+});
 
-$window.smartresize(function() {
-    if (window.matchMedia("screen and (min-width: 768px)").matches) {
-        isMobile = false;
-        console.log('SMR Mobile');
-    } else {
-        console.log('SMR Desktop');
-    }
-})
-
-function runFunctionsBasedOnScreenSize() {
-    if(isMobile) {
-        console.log('runFunctionsBasedOnScreenSize Mobile');
+function checkSize() {
+    if (windowWidth <= 890) {
         mobileFunctions();
-    } else if(!isMobile) {
-        console.log('runFunctionsBasedOnScreenSize Desktop');
-        desktopFunctions();
+    } else {
+        restoreDesktopSlider();
+        initializeDesktopInfo();
     }
 }
 
-function desktopFunctions() {
-    initializeDesktopInfo();
-    restoreDesktopSlider();
-
-    function initializeDesktopInfo() {
-        $('.info-button-trigger').click(function(e) {
-            e.preventDefault();
-            console.log('initializeDesktopInfo');
-            $(this).removeClass('info-button-trigger').addClass('x-button-trigger');
-            $('.sprite-i').animate({ opacity: 0}, 500, function(){
-                $(this).removeClass('sprite-i').addClass('sprite-x').animate({ opacity: 1}, 500);
-                $('.home-text-overlay').animate({ top: '100px'}, 300);
-                $('.home-text-overlay > svg > g > path').css({fill: '#FFF6D6', transition: '.25s'});
-                $('.creative').animate({ opacity: 1 }, 300);
-                setTimeout(function(){
-                    $('.studio').animate({ opacity: 1 }, 300);
-                    $('.home-text-overlay').css('z-index', -3);
-                    $('.information').css('z-index', 0);
-                }, 100);
-            }); 
-            $('.home-slider').animate({ opacity: 0 }, 1200, function(){
-                setTimeout(function(){
-                    $('.info-flex-container').animate({ opacity: 1}, 750);
-                }, 250);
-            });
-        })
+//run resize functions
+$(window).resize(function () {
+    if (windowWidth != $(window).width()) {
+        windowWidth = $(window).width();
+        checkSize();
     }
+});
 
-     function restoreDesktopSlider() {
-         $('.x-button-trigger').click(function(e) {
-             e.preventDefault();
-             console.log('restoreDesktopSlider');
-             $('.sprite-x').animate({ opacity: 0}, 500, function(){
-                $(this).removeClass('sprite-x').addClass('sprite-i').animate({ opacity: 1}, 500);
-                $('.info-flex-container').animate({ opacity: 0}, 750, function(){
-                     setTimeout(function(){
-                         $('.studio').animate({ opacity: 0 }, 300);
-                     }, 250);
-                    $('.creative').animate({ opacity: 0 }, 300);
-                    $('.home-text-overlay > svg > g > path').css({fill: '#000', transition: '.25s'});
-                    $('.home-text-overlay').animate({ top: '50%'}, 300);
-                 });
-             });
-         })
-     }
+function initializeDesktopInfo() {
+    $('.info-button-trigger').on( "click", function(bindClickInfo) {
+        bindClickInfo.preventDefault();
+        console.log('initializeDesktopInfo');
+        $(this).removeClass('info-button-trigger').addClass('x-button-trigger');
+        $('.sprite-i').animate({ opacity: 0}, 500, function(){
+            $(this).removeClass('sprite-i').addClass('sprite-x').animate({ opacity: 1}, 500);
+            $('.home-text-overlay').animate({ top: '100px'}, 300);
+            $('.home-text-overlay > svg > g > path').css({fill: '#FFF6D6', transition: '.25s'});
+            $('.creative').animate({ opacity: 1 }, 300);
+            setTimeout(function(){
+                $('.studio').animate({ opacity: 1 }, 300);
+                $('.home-text-overlay').css('z-index', -3);
+                $('.information').css('z-index', 0);
+            }, 100);
+        }); 
+        $('.home-slider').animate({ opacity: 0 }, 1200, function(){
+            setTimeout(function(){
+                $('.info-flex-container').animate({ opacity: 1}, 750);
+            }, 250);
+        });
+    })
+}
+
+function restoreDesktopSlider() {
+    $('.x-button-trigger').on( "click", function(bindClickX) {
+        bindClickX.preventDefault();
+        console.log('restoreDesktopSlider');
+        $('.sprite-x').animate({ opacity: 0}, 500, function(){
+        $(this).removeClass('sprite-x').addClass('sprite-i').animate({ opacity: 1}, 500);
+        $('.info-flex-container').animate({ opacity: 0}, 750, function(){
+                setTimeout(function(){
+                    $('.studio').animate({ opacity: 0 }, 300);
+                }, 250);
+            $('.creative').animate({ opacity: 0 }, 300);
+            $('.home-text-overlay > svg > g > path').css({fill: '#000', transition: '.25s'});
+            $('.home-text-overlay').animate({ top: '50%'}, 300).css('z-index', 999);
+            $('.home-slider').animate({ opacity: 1 }, 1200);
+            });
+        });
+    })
 }
 
 function mobileFunctions() {
